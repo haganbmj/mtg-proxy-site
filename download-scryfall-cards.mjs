aef972fd9d2b7e783f4a7b18a9c0e950abfb5030
Brendan Hagan <bhaganksu@gmail.com>
2021-08-10 22:35:59 -0400

    misc: add some additional tests and comments to the card script

--- download-scryfall-cards.mjs
+++ download-scryfall-cards.mjs
@@ -69,10 +69,12 @@ const excludedLayouts = [
 ];
 
 const stripped = cards.filter(card => {
+    // Process the exclusions.
     return !excludedSetTypes.includes(card.set_type)
         && !excludedLayouts.includes(card.layout)
         && !excludedSets.includes(card.set);
 }).map(card => {
+    // Then set the high level data necessary to organize the remaining cards.
     return {
         id: card.id,
         oracleId: card.oracle_id,
@@ -96,8 +98,11 @@ const stripped = cards.filter(card => {
 // fs.writeFileSync('./out.json', JSON.stringify(stripped, null, 2));
 
 const minimized = stripped.sort((a, b) => {
+    // From there organize everything by release date in reverse chronological order.
     return Date.parse(a.releaseDate) <= Date.parse(b.releaseDate) ? -1 : 1;
 }).reduce((store, card) => {
+    // And take that and tighten it down as much as possible.
+    // Need to look into gzip more, it might be possible to leave full urls if they get properly compressed out.
     const name = card.name.toLowerCase();
     store.cards[name] = store.cards[name] || [];
     store.cards[name].push({
@@ -113,18 +118,33 @@ const minimized = stripped.sort((a, b) => {
     return store;
 }, { cards: {}, sets: {} });
 
+console.log(`Found ${Object.keys(minimized.cards).length} cards from ${Object.keys(minimized.sets).length} sets.`);
+
+// Run some basic sanity tests.
 assert.equal(minimized.cards['abandon hope']?.length, 1);
 assert.equal(minimized.cards['abandon hope']?.[0].s, 'tmp|107');
 assert.match(minimized.cards['abandon hope']?.[0].f, /^((?!scryfall\.com).)*\.jpg$/);
 
+assert.equal(minimized.cards['lightning dragon']?.length, 4);
+assert.equal(minimized.cards['lightning dragon']?.[0].s, 'pusg|202');
+assert.equal(minimized.cards['lightning dragon']?.[1].s, 'usg|202');
+assert.equal(minimized.cards['lightning dragon']?.[2].s, 'prm|32196');
+assert.equal(minimized.cards['lightning dragon']?.[3].s, 'vma|177');
+assert.equal(minimized.cards['lightning dragon']?.[0].d, undefined);
+assert.equal(minimized.cards['lightning dragon']?.[1].d, undefined);
+assert.equal(minimized.cards['lightning dragon']?.[2].d, 1);
+assert.equal(minimized.cards['lightning dragon']?.[3].d, 1);
+assert.equal(minimized.cards['lightning dragon']?.[0].p, 1);
+assert.equal(minimized.cards['lightning dragon']?.[1].p, undefined);
+assert.equal(minimized.cards['lightning dragon']?.[2].p, 1);
+assert.equal(minimized.cards['lightning dragon']?.[3].p, undefined);
+
 assert.equal(minimized.sets['tmp'], 'Tempest');
 
-// I think the next major minimization that could be done would have to be with the set names.
-// Could alias those and pull them out to a seperate file for mapping against. Saves 880kb pre-whitespace stripping.
+assert(Object.keys(minimized.cards).length > 20000);
+assert(Object.keys(minimized.sets).length > 500);
+
 // fs.writeFileSync('./min-pretty.json', JSON.stringify(minimized, null, 2));
 fs.writeFileSync('./data/cards-minimized.json', JSON.stringify(minimized, null, 2));
 
 console.log('Finished writing minimized card list.');
-// 4072kb with `code number`
-// 4453kb with s, n
-// 4938kb with `name (number)`
