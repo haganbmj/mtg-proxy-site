b37a466813e43e69747ab0cf87e29a1a665e22ad
Brendan Hagan <bhaganksu@gmail.com>
2021-09-24 17:30:28 -0400

    misc: revise quotation presentation
    and add some quotation marks to the pain quote

--- src/components/ArnoldsApproval.vue
+++ src/components/ArnoldsApproval.vue
@@ -1,19 +1,10 @@
 <template>
-    <div class="columns col-9 col-sm-12 col-mx-auto">
-        <template v-if="approval">
-            <span class="h6 p-centered"><i>Your list doesn't contain Griselbrand! Good job.</i></span>
-            <blockquote>
-                <p>Life may be full of pain, but that’s not an excuse to give up.</p>
-                <cite>- Arnold Schwarzenegger</cite>
-            </blockquote>
-        </template>
-        <template v-else-if="approval === false">
-            <i class="h6 p-centered">Your list contains Griselbrand. How disappointing.</i>
-            <blockquote>
-                <p>"What is the point of being on this Earth if you are going to be like everyone else?"</p>
-                <cite>- Arnold Schwarzenegger (on Griselbrand)</cite>
-            </blockquote>
-        </template>
+    <div class="columns col-9 col-sm-12 col-mx-auto" v-if="approvalContent !== undefined">
+        <span class="h6 p-centered"><i>{{ approvalContent.header }}</i></span>
+        <blockquote>
+            <p>{{ approvalContent.quote }}</p>
+            <cite>- {{ approvalContent.attribution }}</cite>
+        </blockquote>
     </div>
 </template>
 
@@ -26,16 +17,24 @@ export default {
         },
     },
     computed: {
-        approval() {
+        approvalContent() {
             if (this.cards.length === 0) {
                 return undefined;
             }
 
-            const griselbrand = this.cards.find(card => {
-                return card.name === 'griselbrand';
-            });
-
-            return griselbrand ? false : true;
+            if (this.cards.find(card => card.name === 'griselbrand')) {
+                return {
+                    header: `Your list contains Griselbrand. How disappointing.`,
+                    quote: `"What is the point of being on this Earth if you are going to be like everyone else?"`,
+                    attribution: `Arnold Schwarzenegger (on Griselbrand)`,
+                };
+            } else {
+                return {
+                    header: `Your list doesn't contain Griselbrand! Good job.`,
+                    quote: `"Life may be full of pain, but that’s not an excuse to give up."`,
+                    attribution: `Arnold Schwarzenegger`,
+                };
+            }
         },
     },
 }
