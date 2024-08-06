d181ecfb2ca1b978668238d169ff3f60e3897555
Brendan Hagan <bhaganksu@gmail.com>
2024-08-05 21:41:34 -0400

    feat: minimally introduce sass compilation

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import 'spectre.css'
-import 'spectre.css/dist/spectre-icons.css'
+import '../assets/style.scss'
 
 createApp(App).mount('#app')
